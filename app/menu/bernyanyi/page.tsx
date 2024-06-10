9f77ccd628c578ade96a61256877808bbd8df495
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-10 17:43:14 +0700

    change video with iframe

--- app/menu/bernyanyi/page.tsx
+++ app/menu/bernyanyi/page.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Page () {
+export default function Page() {
     return (
-        <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+        <main className="min-h-screen flex flex-col items-center justify-center gap-4 overflow-auto">
             <Link href="/menu" className="absolute top-5 left-5">
                 <Image src="/back.svg" alt="home" width={50} height={50} />
             </Link>
-            <video width="768" height="432" controls className="mx-auto rounded-3xl outline outline-offset-4 outline-4 outline-yellow-400">
-                <source  src="/videos/bernyanyi.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <div>
+                <iframe width="853" height="480" 
+                    className="mx-auto rounded-3xl outline outline-offset-4 outline-4 outline-yellow-400"
+                    src="https://www.youtube.com/embed/nNwjhGtgdSQ?si=MyCJKdm2OIPCCldk" 
+                    title="Kepala Pundak Lutut Kaki" frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </div>
         </main>
     )
 }
