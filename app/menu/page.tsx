363d83eee7e3796debb45b357ee5140919368f39
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-10 10:36:57 +0700

    chore: Update metadata and favicon for Belajar Anatomi

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -12,8 +12,11 @@ export default function Menu() {
             <Image src="/belajar.png" alt="Belajar" width={300} height={300} />
             <Link
                 href="/menu/belajar"
-                className="bg-yellow-400 py-2 px-4 outline outline-offset-2 outline-white rounded-md hover:bg-yellow-600 hover:scale-110 transition-all duration-300 hover:outline-green-700 text-2xl font-bold text-black"
+                className="flex items-center w-max mx-auto gap-4 bg-yellow-400 py-2 px-6 outline outline-offset-2 outline-white rounded-full hover:bg-yellow-600 hover:scale-110 transition-all duration-300 hover:outline-green-700 text-2xl font-bold text-black"
             >
+              <svg className="text-white" xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
+              </svg>
                 Belajar
             </Link>
         </div>
@@ -21,8 +24,11 @@ export default function Menu() {
             <Image src="/bermain.png" alt="Bermain" width={300} height={300} />
             <Link
                 href="/menu/bermain"
-                className="bg-yellow-400 py-2 px-4 outline outline-offset-2 outline-white rounded-md hover:bg-yellow-600 hover:scale-110 transition-all duration-300 hover:outline-green-700 text-2xl font-bold text-black"
+                className="flex items-center w-max mx-auto gap-4 bg-yellow-400 py-2 px-6 outline outline-offset-2 outline-white rounded-full hover:bg-yellow-600 hover:scale-110 transition-all duration-300 hover:outline-green-700 text-2xl font-bold text-black"
             >
+              <svg className="text-white" xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
+              </svg>
                 Bermain
             </Link>
         </div>
