f14a51b7d0d318a88f3619f29ae097b7ed89a33e
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-12 21:40:47 +0700

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      {/*<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />*/}
       <body className={inter.className}>{children}</body>
     </html>
   );
