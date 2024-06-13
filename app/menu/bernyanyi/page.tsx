84314df44415fe11f9641df2c5dae6e0b80d1c5c
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-13 22:38:20 +0700

    refactor: Update bernyanyi page with carousel and custom arrows

--- app/menu/bernyanyi/page.tsx
+++ app/menu/bernyanyi/page.tsx
@@ -38,44 +38,52 @@ export default function Page() {
     };
     return (
         <main className="min-h-screen flex flex-col items-center justify-center gap-4 overflow-auto">
-            <Link href="/menu" className="absolute top-5 left-5">
-                <Image src="/back.svg" alt="home" width={50} height={50} />
-            </Link>
+
             <div className="carousel w-full">
                 <div id="slide1" className="w-full py-2 relative carousel-item">
-                    <iframe width="853" height="480"
-                            className="mx-auto rounded-3xl outline outline-offset-4 outline-4 outline-yellow-400"
-                            src="https://www.youtube.com/embed/PeH7Jx5P17o?si=yE5mIUOP9WiuvF93"
-                            title="Kepala Pundak Lutut Kaki" frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">
+                    <div className="video-container mx-auto w-full">
+                        <iframe width="853" height="480"
+                                className="mx-auto rounded-3xl outline outline-offset-4 outline-4 outline-yellow-400"
+                                src="https://www.youtube.com/embed/PeH7Jx5P17o?autoplay=1&si=yE5mIUOP9WiuvF93"
+                                title="Kepala Pundak Lutut Kaki" frameBorder="0"
+                                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-7">
+                        <a href="#slide2" className="btn btn-circle bg-yellow-500 text-black hover:bg-yellow-600">
                             ❮
                         </a>
-                        <a href="#slide2" className="btn btn-circle">
+                        <a href="#slide2" className="btn btn-circle bg-yellow-500 text-black hover:bg-yellow-600">
                             ❯
                         </a>
                     </div>
                 </div>
 
                 <div id="slide2" className="w-full py-2 relative carousel-item">
-                    <iframe width="853" height="480"
-                            className="mx-auto rounded-3xl outline outline-offset-4 outline-4 outline-yellow-400"
-                            src="https://www.youtube.com/embed/t13g4HkV-ZU?si=8XG3aL2bgGWrmjFu"
-                            title="Kepala Pundak Lutut Kaki" frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">
+                    <div className="video-container mx-auto">
+                        <iframe width="853" height="480"
+                                className="mx-auto rounded-3xl outline outline-offset-4 outline-4 outline-yellow-400"
+                                src="https://www.youtube.com/embed/t13g4HkV-ZU?autoplay=1&si=8XG3aL2bgGWrmjFu"
+                                title="Kepala Pundak Lutut Kaki" frameBorder="0"
+                                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-7">
+                        <a href="#slide1" className="btn btn-circle bg-yellow-500 text-black hover:bg-yellow-600 border">
                             ❮
                         </a>
-                        <a href="#slide1" className="btn btn-circle">
+                        <a href="#slide1" className="btn btn-circle bg-yellow-500 text-black hover:bg-yellow-600">
                             ❯
                         </a>
                     </div>
                 </div>
             </div>
+            <Link
+                href="/menu"
+                className="bg-yellow-400 p-2 rounded-full hover:bg-yellow-600 hover:scale-110 transition-all duration-300"
+            >
+                <Image src="/back.svg" alt="home" width={50} height={50}/>
+            </Link>
         </main>
     )
 }
