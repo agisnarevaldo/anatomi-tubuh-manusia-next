444da99c048fcbc2521853eddcf50b87ea9cf0d0
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-30 21:08:55 +0700

    refactor: Update bermain page with cleanup and speech synthesis cancellation

--- app/menu/bermain/page.tsx
+++ app/menu/bermain/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import Quiz from "@/app/component/Quiz";
+import {useEffect} from "react";
 
-export default function Menu() {
+export default function Bermain() {
+    useEffect(() => {
+        return () => {
+            window.speechSynthesis.cancel();
+        }
+    }, []);
     return (
         <main className="flex min-h-screen items-center justify-center gap-8">
             <Link href="/menu" className="absolute top-5 left-5">
